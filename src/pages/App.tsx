ea17e53b0c72c138bf35f84bee2d22176e878b54
Helder Esteves <helder.jbe@gmail.com>
2023-04-01 19:30:48 +0100

    Add FAB button with styling.

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <button className="floating-button">
+        <div className="button-border">GIF</div>
+      </button>
+    </div>
+  );
+}
+
+export default App;
